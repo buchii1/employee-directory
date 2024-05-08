a17ef67176d3dc0fa77c91386b96ea1d576c6fd1
buchii <oko22007@byui.edu>
2024-05-08 16:44:13 +0100

    add header files

--- snippets/header.php
+++ snippets/header.php
@@ -1,5 +1,5 @@
 <a href="/employee-directory"><h4>Employee MS</h4></a>
 <div class="header">
-    <a href="/login">Login</a>
-    <a href="/sign-up">Sign up</a>
+    <a href="/employee-directory/directory/?action=add-department">Add Department</a> |
+    <a href="/employee-directory/directory/?action=add-employee">Add Employee</a>
 </div>
--- snippets/navigation.php
+++ snippets/navigation.php
@@ -2,5 +2,6 @@
     <!-- <li><a href="/employee-directory">Home</a></li> -->
     <li><a href="/employees">Employees</a></li>
     <li><a href="/departments">Departments</a></li>
+    <li><a href="/employee-directory/directory/?action=add-department">Add Department</a></li>
     <li><a href="/employee-directory/directory/?action=add-employee">Add Employee</a></li>
 </ul>
